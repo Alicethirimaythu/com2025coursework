3e3f265aa85a7a4c8e6b1639bf93069d00291344
Thiri May Thu <tt00562@surrey.ac.uk>
2021-12-01 16:15:24 +0000

    Added plants title index

--- db/migrate/20211201153532_create_plantopia.rb
+++ db/migrate/20211201153532_create_plantopia.rb
@@ -1,7 +1,7 @@
 class CreatePlantopia < ActiveRecord::Migration[5.2]
   def change
     create_table :plantopia do |t|
-      t.string :name
+      t.string :name, null:false
       t.text :description
 
       t.timestamps
