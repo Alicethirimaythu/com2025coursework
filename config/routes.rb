919b18e598a7ab30ea6fc7c45f4ca4c06ce710ea
Thiri May Thu <tt00562@surrey.ac.uk>
2021-12-01 20:30:25 +0000

    design the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
   get 'home', to:'home#home'
+  get 'plantopia', to: 'home#plantopia'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
